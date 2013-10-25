a89a8f7c7a2f1a67209bad95312b04fb96441afa
Sercheo <sercheo@ubuntu.ubuntu-domain>
2013-10-25 08:45:02 -0400

    Animacion de titulos y contenedor

--- js/script.js
+++ js/script.js
@@ -1,11 +1,18 @@
 $(document).ready(function() {
-  $("#navigation span").on('click',function() {
+    $("#navigation span").on('click',function() {
         $('.container div').removeClass("opaque");
         $('#navigation span').removeClass("selected");
-
-        var $input = $(this);
-        $('.'+$input.attr( "id" )).addClass("opaque");
+        
+        var $container = $(this);
+        
+        
+        $('.'+$container.attr("id")+' h1').css('margin-left','100%')
+        $('.'+$container.attr("id")+' h1').animate({
+            marginLeft: "0px"
+        }, 700);
+        
+        $('.'+$container.attr("id")).addClass("opaque");
         $(this).addClass("selected");
-
-  });
+        
+    });
 });
